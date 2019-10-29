3ef6342570a81a566abafd0867beb4ff242a451a
Jeff Huang <jeffshuang2@gmail.com>
2019-10-29 13:25:16 -0700

    Added switch statement to grab first argument and created function to call Bands In Town API

--- liri.js
+++ liri.js
@@ -9,6 +9,55 @@ var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 
-var bandsInTownUrl = "https://rest.bandsintown.com/artists/" + command + "/events?app_id=codingbootcamp";
+if (process.argv.length > 3) {
+    var input = process.argv.slice(3).join("");
+    console.log(input);
+}
 
-console.log(bandsInTownUrl);
+switch (command) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThis();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doThis();
+        break;
+    default:
+        console.log("You have not entered a valid statement.");
+}
+
+function concertThis() {
+    var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL).then(
+        function(response) {
+            console.log(response.data);
+        }
+    )
+    .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("----------------Data----------------");
+          console.log(error.response.data);
+          console.log("----------------Status----------------");
+          console.log(error.response.status);
+          console.log("----------------Status----------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
+};
