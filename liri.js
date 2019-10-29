64e25b0be1b6de3dbeb11a959279e31917791c7a
Jeff Huang <jeffshuang2@gmail.com>
2019-10-29 12:47:13 -0700

    Installed packages.

--- liri.js
+++ liri.js
@@ -1,5 +1,14 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+
+var bandsInTownUrl = "https://rest.bandsintown.com/artists/" + command + "/events?app_id=codingbootcamp";
+
+console.log(bandsInTownUrl);
